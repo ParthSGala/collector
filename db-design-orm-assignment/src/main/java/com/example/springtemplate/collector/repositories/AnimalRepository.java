2db5f89198db866c6a2bd0c92d7ff581f0f4f749
ParthSGala <71418093+ParthSGala@users.noreply.github.com>
2021-12-15 08:31:07 -0500

    Delete db-design-orm-assignment directory

--- db-design-orm-assignment/src/main/java/com/example/springtemplate/collector/repositories/AnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springtemplate.collector.repositories;
-
-import com.example.springtemplate.collector.models.Animal;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface AnimalRepository
-        extends CrudRepository<Animal, Integer> {
-}
